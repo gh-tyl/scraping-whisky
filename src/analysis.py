86b0ceff8f9173d250f64288302aa39a77fd05dc
Tyler Inari <github@al.tyler.jp>
2022-11-16 13:36:34 -0800

    manage data

--- src/analysis.py
+++ src/analysis.py
@@ -1,99 +1,153 @@
 # %%
-import pandas as pd
+# import pandas as pd
 
-# %%
-path = "./data/WhiskyDatabase.csv"
-df = pd.read_csv(path)
-# %%
-df = df.dropna()
-df = df.reset_index(drop=True)
-# write not nan df to csv file
-path = "./data/WhiskyDatabase_not_nan.csv"
-df.to_csv(path, index=False)
+# # %%
+# path = "./data/WhiskyDatabase.csv"
+# df = pd.read_csv(path)
+# # %%
+# df = df.dropna()
+# df = df.reset_index(drop=True)
+# # write not nan df to csv file
+# path = "./data/WhiskyDatabase_not_nan.csv"
+# df.to_csv(path, index=False)
 
-# %%
-def create_primary_score(df):
-    # get columns
-    def _create_dict(column: str) -> dict:
-        # create a dictionary of the column
-        column_dict = df[column].value_counts().to_dict()
-        column_dict = {
-            k: v
-            for k, v in sorted(
-                column_dict.items(), key=lambda item: item[1], reverse=True
-            )
-        }
-        column_dict = {k: i for i, k in enumerate(column_dict.keys())}
-        return column_dict
+# # %%
+# def create_primary_score(df):
+#     # get columns
+#     def _create_dict(column: str) -> dict:
+#         # create a dictionary of the column
+#         column_dict = df[column].value_counts().to_dict()
+#         column_dict = {
+#             k: v
+#             for k, v in sorted(
+#                 column_dict.items(), key=lambda item: item[1], reverse=True
+#             )
+#         }
+#         column_dict = {k: i for i, k in enumerate(column_dict.keys())}
+#         return column_dict
 
-    primary_score = "Primary Score"
-    columns = df.columns
-    drop_columns = ["Whisky", "Meta Critic", "STDEV", "#", "Primary Score"]
-    columns = [column for column in columns if column not in drop_columns]
-    df[primary_score] = [0] * len(df)
-    for column in columns:
-        column_dict = _create_dict(column)
-        score_list = []
-        print(column)
-        print(column_dict)
-        for key, value in df[column].items():
-            score_list.append(column_dict[value])
-        df[primary_score] += score_list
-        # break
+#     primary_score = "Primary Score"
+#     columns = df.columns
+#     drop_columns = ["Whisky", "Meta Critic", "STDEV", "#", "Primary Score"]
+#     columns = [column for column in columns if column not in drop_columns]
+#     df[primary_score] = [0] * len(df)
+#     for column in columns:
+#         column_dict = _create_dict(column)
+#         score_list = []
+#         print(column)
+#         print(column_dict)
+#         for key, value in df[column].items():
+#             score_list.append(column_dict[value])
+#         df[primary_score] += score_list
+#         # break
 
-    return df
+#     return df
 
 
-# %%
-df = create_primary_score(df)
-# %%
-df
-# %%
-df = df.sort_values(by=["Primary Score"], ascending=False)
-df = df.reset_index(drop=True)
-# %%
-df_200 = df[:250]
+# # %%
+# df = create_primary_score(df)
+# # %%
+# df
+# # %%
+# df = df.sort_values(by=["Primary Score"], ascending=False)
+# df = df.reset_index(drop=True)
+# # %%
+# df_200 = df[:250]
 
-# %%
-df_200.groupby("Country").count()
+# # %%
+# df_200.groupby("Country").count()
 
-# %%
-# write 100 random rows to csv file
-path = "./data/WhiskyDatabase_250.csv"
-df_200.to_csv(path, index=False)
+# # %%
+# # write 100 random rows to csv file
+# path = "./data/WhiskyDatabase_250.csv"
+# df_200.to_csv(path, index=False)
 
-# %%
-import pandas as pd
+# # %%
+# import pandas as pd
 
+# # read csv file
+# path = "./data/WhiskyDatabase_200_with_image.csv"
+# df = pd.read_csv(path)
+# # %%
+# # drop rows with nan
+# df = df.dropna()
+# df = df.reset_index(drop=True)
+# # %%
+# # drop column Primary Score
+# df = df.drop(columns=["Primary Score"])
+# # %%
+# # convert Cost to int
+# # $ is for whiskies <$30 CAD
+# # $$ for whiskies between $30~$50 CAD
+# # $$$ for whiskies between $50-$70 CAD
+# # $$$$ for whiskies between $70~$125 CAD
+# # $$$$$ for whiskies between $125~$300 CAD
+# # $$$$$+ refers to all whiskies >$300 CAD.
+# df["Cost"] = df["Cost"].replace(
+#     {"$": 30, "$$": 50, "$$$": 70, "$$$$": 125, "$$$$$": 300, "$$$$$+": 300}
+# )
+
+# # %%
+# # write csv file
+# path = "./data/WhiskyDatabase_200_replace_cost.csv"
+# df.to_csv(path, index=False)
+# %%
 # read csv file
-path = "./data/WhiskyDatabase_200_with_image.csv"
+import pandas as pd
+
+path = "./data/WhiskyDatabase_200_replace_cost.csv"
 df = pd.read_csv(path)
+
+# add id column
+df["id"] = [i for i in range(len(df))]
+
 # %%
-# drop rows with nan
-df = df.dropna()
-df = df.reset_index(drop=True)
-# %%
-# drop column Primary Score
-df = df.drop(columns=["Primary Score"])
-# %%
-# convert Cost to int
-# $ is for whiskies <$30 CAD
-# $$ for whiskies between $30~$50 CAD
-# $$$ for whiskies between $50-$70 CAD
-# $$$$ for whiskies between $70~$125 CAD
-# $$$$$ for whiskies between $125~$300 CAD
-# $$$$$+ refers to all whiskies >$300 CAD.
-df["Cost"] = df["Cost"].replace(
-    {"$": 30, "$$": 50, "$$$": 70, "$$$$": 125, "$$$$$": 300, "$$$$$+": 300}
+# rename columns
+df = df.rename(
+    columns={
+        "id": "id",
+        "Whisky": "name",
+        "Meta Critic": "meta_critic",
+        "STDEV": "stdev",
+        "#": "reviewers_num",
+        "Cost": "price",
+        "Class": "class",
+        "Super Cluster": "super_cluster",
+        "Cluster": "cluster",
+        "Country": "country",
+        "Type": "type",
+        "image_path_0": "image_path_0",
+        "image_path_1": "image_path_1",
+        "image_path_2": "image_path_2",
+    }
 )
 
 # %%
-# write csv file
-path = "./data/WhiskyDatabase_200_replace_cost.csv"
-df.to_csv(path, index=False)
+# change column order
+df = df[
+    [
+        "id",
+        "name",
+        "price",
+        "country",
+        "type",
+        "image_path_0",
+        "image_path_1",
+        "image_path_2",
+        "class",
+        "super_cluster",
+        "cluster",
+        "meta_critic",
+        "stdev",
+        "reviewers_num",
+    ]
+]
+
 # %%
 # convert csv to json
 import json
 
 path = "./data/WhiskyDatabase_200_replace_cost.json"
 df.to_json(path, orient="records")
+
+# %%
